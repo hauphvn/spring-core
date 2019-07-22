072f9fc546478984a5aade092f87ace5f424bdab
Hau <hau.pham@gigacover.com>
2019-07-22 10:14:00 +0700

    Autowired, setter injection, constructor injection

--- src/main/java/edu/springcore/TestRunMain.java
+++ src/main/java/edu/springcore/TestRunMain.java
@@ -1,6 +1,7 @@
 package edu.springcore;
 
 import edu.springcore.model.Student;
+import edu.springcore.service.StudentService;
 import edu.springcore.service.StudentServiceEmpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,9 +11,15 @@ import java.util.List;
 public class TestRunMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        StudentServiceEmpl studentServiceEmpl = new StudentServiceEmpl();
+
+        //Khong su dung bean
+        /*StudentServiceEmpl studentServiceEmpl = new StudentServiceEmpl();
         List<Student> students = studentServiceEmpl.getAll();
-        System.out.println(students);
+        System.out.println(students);*/
+
+        //Su dung bean
+        StudentService studentService = context.getBean("studentService", StudentService.class);
+        System.out.println(studentService.getAll());
 
     }
 }
